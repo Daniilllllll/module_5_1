149376a8d72c1c56fc52a811e63a68d6730e3456
Daniilllllll <privetdanilka22@gmail.com>
2024-11-09 16:10:39 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+
+
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+
+    def go_to(self, new_floor):
+        floor = 0
+        print(f"{self.name} имеет {self.number_of_floors} этажей, едем на {new_floor}!")
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print(f"В здании {self.name} такого этажа не существует!")
+        else:
+            for floor in range(new_floor):
+                print(floor + 1)
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
+
+
+
+
+
+
+
